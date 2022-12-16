673457182d1597abc3662b424e4b4742b84b41ea
SHctt <SHcttMBG@protonmail.com>
2022-12-16 16:13:10 +0800

    created():组件的生命周期方法

--- src/app/app.vue
+++ src/app/app.vue
@@ -9,5 +9,14 @@ export default {
       name: 'VUE lesson 2-2、 data()方法返回数据',
     };
   },
+
+  created() {
+    console.log('3秒后，本节标题将会替代原有标题');
+
+    setTimeout(() => {
+      this.name =
+        'VUE lesson 2-3、 created()组件被创建后这个生命周期所执行的方法';
+    }, 3000);
+  },
 };
 </script>
