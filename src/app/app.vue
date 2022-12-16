7b8c92af48ea8174012ec9931734f5bea65a6bcb
SHctt <SHcttMBG@protonmail.com>
2022-12-16 17:07:01 +0800

    computed:组件里的计算属性

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,28 +1,25 @@
 <template>
-  <h3>{{ name }}</h3>
+  <h3>{{ processStatus }}</h3>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      name: 'VUE lesson 2-4、 methods:在methods对象中定义方法',
+      name: 'computed:组件里的计算属性',
     };
   },
 
-  created() {
-    console.log('3秒后，调用setName（）这个方法定义的标题');
-
-    this.setName();
+  computed: {
+    processStatus() {
+      return this.name === 'computed'
+        ? 'VUE lesson 2-5、'
+        : 'VUE lesson 2-5、 computed:组件里的计算属性';
+    },
   },
 
-  methods: {
-    setName() {
-      setTimeout(() => {
-        this.name =
-          'VUE lesson 2-4、 在methods:对象中定义setName（）方法，被created（）这个生命周期调用';
-      }, 3000);
-    },
+  created() {
+    console.log('App组件已经被创建');
   },
 };
 </script>
