ed00e9b83a8a939a42d47794ee3ef30cfe3598a3
SHctt <SHcttMBG@protonmail.com>
2022-12-16 16:31:16 +0800

    methods:在methoss对象中定义方法

--- src/app/app.vue
+++ src/app/app.vue
@@ -6,17 +6,23 @@
 export default {
   data() {
     return {
-      name: 'VUE lesson 2-2、 data()方法返回数据',
+      name: 'VUE lesson 2-4、 methods:在methods对象中定义方法',
     };
   },
 
   created() {
-    console.log('3秒后，本节标题将会替代原有标题');
+    console.log('3秒后，调用setName（）这个方法定义的标题');
 
-    setTimeout(() => {
-      this.name =
-        'VUE lesson 2-3、 created()组件被创建后这个生命周期所执行的方法';
-    }, 3000);
+    this.setName();
+  },
+
+  methods: {
+    setName() {
+      setTimeout(() => {
+        this.name =
+          'VUE lesson 2-4、 在methods:对象中定义setName（）方法，被created（）这个生命周期调用';
+      }, 3000);
+    },
   },
 };
 </script>
