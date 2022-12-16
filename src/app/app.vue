5644f6f0f051a5c0fe4a122d851ed8fff12bbef7
SHctt <SHcttMBG@protonmail.com>
2022-12-16 18:28:46 +0800

    watch 属性：vue 组件里的数据监视器

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,25 +1,36 @@
 <template>
-  <h3>{{ processStatus }}</h3>
+  <div>
+    <h3>{{ name }}</h3>
+    {{ processStatus }}
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      name: 'computed:组件里的计算属性',
+      name: '2.6-watch 属性：vue组件里的数据监视器',
+      dat: '当前状态',
     };
-  },
-
-  computed: {
-    processStatus() {
-      return this.name === 'computed'
-        ? 'VUE lesson 2-5、'
-        : 'VUE lesson 2-5、 computed:组件里的计算属性';
-    },
   },
 
   created() {
     console.log('App组件已经被创建');
+    setTimeout(() => {
+      this.dat = '变一下';
+    }, 3000);
+  },
+
+  computed: {
+    processStatus() {
+      return this.dat === '状态' ? '当前状态' : '初始化状态';
+    },
+  },
+
+  watch: {
+    dat(newDat, oldDat) {
+      console.log(`dat的值发生了变化: ${oldDat} -> ${newDat}`);
+    },
   },
 };
 </script>
