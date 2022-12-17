96e6f627ccb7730d91a445b6fdca908cd3ed1eb2
SHctt <SHcttMBG@protonmail.com>
2022-12-17 19:11:22 +0800

    $refs：引用模板

--- src/app/component/UserLogin.vue
+++ src/app/component/UserLogin.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
     <div>用户登陆</div>
-    <input type="text" name="" id="" />
-    <input type="text" name="" id="" />
+    <input type="text" ref="userName" />
+    <input type="text" />
   </div>
 </template>
+
+<script>
+export default {
+  mounted() {
+    console.log(this.$refs);
+    this.$refs.userName.focus();
+  },
+};
+</script>
--- src/app/component/UserRegister.vue
+++ src/app/component/UserRegister.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div>用户注册</div>
-    <input type="text" name="" id="" />
-    <input type="text" name="" id="" />
+    <input type="text" />
+    <input type="text" />
   </div>
 </template>
