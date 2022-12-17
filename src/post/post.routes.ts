726e57128f67711fc7dede0c84fe15f9f9e2d686
SHctt <SHcttMBG@protonmail.com>
2022-12-18 02:12:54 +0800

    动态路由（带地址参数的路由）

--- src/post/post.routes.ts
+++ src/post/post.routes.ts
@@ -1,11 +1,17 @@
 import { RouteRecordRaw } from 'vue-router';
 import postIndex from './index/index.vue';
+import postShow from './show/post-show.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     name: 'postIndex',
     path: '/posts',
     component: postIndex,
+  },
+  {
+    name: 'postShow',
+    path: '/posts/:postId',
+    component: postShow,
   },
 ];
 
---
+++ src/post/show/post-show.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>内容详情</div>
+</template>
+
+<script>
+export default {
+  created() {
+    console.log(this.$route.params.postId);
+  },
+};
+</script>
