afa881879cd07e479a9002e8f4802370c18afc38
SHctt <SHcttMBG@protonmail.com>
2022-12-20 16:14:59 +0800

    JavaScript：Proxy 与 Reflect

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Vue.js 前端应用 #8： 反应系统
 
 学会使用 Vue 框架新的组合式接口创建 Vue 组件。
+
+## 8.1 JavaScript：Proxy 与 Reflect
+
+在 JavaScript 语言里，你可以基于一个对象去创建一个 Proxy 对象，这样你就可以拦截对这个对象的一些操作了，比如访问或者设置对象属性的时候，你可以做一些额外的检查。
+
+这里我们可以在这个 Vue 组件里试一下 JavaScript 的 Proxy。
--- src/app/app.vue
+++ src/app/app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h3>{{ name }}</h3>
+    <h3>{{ mainTitle }}</h3>
   </div>
 </template>
 
@@ -8,8 +8,45 @@
 export default {
   data() {
     return {
-      name: 'Vue.js 前端应用 #8： 反应系统',
+      mainTitle: 'Vue.js 前端应用 #8： 反应系统',
     };
+  },
+
+  created() {
+    const user = {
+      name: 'Scatty',
+      lessonOne: 'JavaScript：Proxy 与 Reflect',
+    };
+
+    const handler = {
+      // 通过get这个方法，来获取被代理对象的数据
+      // get 方法有个默认的行为，接收一个 target 参数，还有一个 property 参数。
+      // target 就是对象，property 就是要访问的对象里的某个属性的名字。
+      get(target, property) {
+        return target[property];
+      },
+
+      //通过set这个方法，去设置被代理对象的数据
+      set(target, property, value) {
+        if (property === 'name') {
+          if (value.length > 10) {
+            throw new Error('超出字数限制');
+          }
+        }
+        Reflect.set(target, property, value);
+      },
+    };
+
+    // 定义userProxy做为一个新的proxy，代理user这个对象，并对这个对象添加handler方法
+    const userProxy = new Proxy(user, handler);
+
+    try {
+      userProxy.name = 'yumfok is a foolish women';
+    } catch (error) {
+      console.log('error:', error.message);
+    }
+
+    console.log(userProxy.name);
   },
 };
 </script>
