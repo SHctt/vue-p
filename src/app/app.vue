f9737e0ac61ed812c7c92ff9115f6a3cf04a9d4d
SHctt <SHcttMBG@protonmail.com>
2022-12-20 17:42:26 +0800

    声明具有反应特性的数据（ref）

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
-    <h3 @click="title = 'Composition API：组合式接口'">
+    <h3 @click="title = '声明具有反应特性的数据（ref）'">
       {{ title }}
     </h3>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
 export default {
   setup() {
-    const title = 'Vue.js 前端应用 #8： 反应系统';
+    const title = ref('Vue.js 前端应用 #8： 反应系统');
+    // 先在控制台看看title
+    console.log(title);
+    // console.log(title.value);
+
     return {
-      title,
+      title, //绑定输出的title，vue会自动处理成为输出title.value
     };
   },
 };
