72f9d0ffb02a73730ef1273d4da0aaa826fa26fd
SHctt <SHcttMBG@protonmail.com>
2022-12-21 01:55:16 +0800

    安装与使用 HTTP 客户端请求服务端接口（axios）

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,15 +1,30 @@
 <template>
   <div>
     <h3>{{ title }}</h3>
+    <div v-for="(post, index) in postsList" :key="index">
+      <div>
+        <small>{{ index + 1 }} - </small> {{ post.title }} --
+        {{ post.content }} --- {{ post.user.name }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   data() {
     return {
       title: '前端应用 #9：请求接口',
+      postsList: [],
     };
+  },
+
+  created() {
+    axios.get('http://localhost:3001/posts').then((response) => {
+      this.postsList = response.data;
+      console.log(this.postsList);
+    });
   },
 };
 </script>
