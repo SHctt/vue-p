36f7c7ae080122386d24e5658f9848198cd76145
SHctt <SHcttMBG@protonmail.com>
2022-12-20 17:56:18 +0800

    声明具有反应特性的数据（reactive）

--- src/app/app.vue
+++ src/app/app.vue
@@ -3,20 +3,26 @@
     <h3 @click="title = '声明具有反应特性的数据（ref）'">
       {{ title }}
     </h3>
+    <div @click="user.name = 'rogolezhou'">{{ user.name }}</div>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 export default {
   setup() {
     const title = ref('Vue.js 前端应用 #8： 反应系统');
-    // 先在控制台看看title
+
+    const user = reactive({
+      name: 'yumfok',
+    });
     console.log(title);
+    console.log(user);
     // console.log(title.value);
 
     return {
-      title, //绑定输出的title，vue会自动处理成为输出title.value
+      title,
+      user, //让组件可以绑定输出user
     };
   },
 };
