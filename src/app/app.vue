65f63e171d3f807e3733f1bb7ea4fc66bfd73d55
SHctt <SHcttMBG@protonmail.com>
2022-12-22 22:48:16 +0800

    在请求里配置头部数据

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <h3>{{ title }}</h3>
+    <h3>{{ projectTitle }}</h3>
+    <div @keyup.enter="createPost">
+      <input type="text" v-model="newPost.title" />
+      <input type="text" v-model="newPost.content" />
+      <!-- <button @click="createPost">提交新文章</button> -->
+    </div>
     <div v-for="(post, index) in postsList" :key="index">
       <div>
         <small>{{ index + 1 }} - </small> {{ post.title }} --
@@ -16,7 +21,7 @@ import { appPostsClient } from '@/app/app.service';
 export default {
   data() {
     return {
-      title: '前端应用 #9：请求接口',
+      projectTitle: '前端应用 #9：请求接口',
       postsList: [],
       errorMessage: '',
       user: {
@@ -24,17 +29,45 @@ export default {
         password: '123123',
       },
       token: '',
+      newPost: { title: '', content: '' },
     };
   },
 
+  methods: {
+    async getPost() {
+      try {
+        const response = await appPostsClient.get('/posts');
+        this.postsList = response.data;
+        // console.log(response.data);
+      } catch (error) {
+        this.errorMessage = error.message;
+      }
+    },
+
+    async createPost() {
+      try {
+        const response = await appPostsClient.post('/posts', this.newPost, {
+          headers: {
+            // 添加用户的头部信息
+            Authorization: `Bearer ${this.token}`,
+          },
+        });
+        if (response) {
+          console.log(response.data);
+          // 重置表单
+          this.newPost = {};
+          // 立刻更新列表
+          this.getPost();
+        }
+      } catch (error) {
+        this.errorMessage = error.message;
+      }
+    },
+  },
+
   async created() {
-    try {
-      const response = await appPostsClient.get('/posts');
-      this.postsList = response.data;
-      // console.log(appPostsClient);
-    } catch (error) {
-      this.errorMessage = error.message;
-    }
+    // 初始化列表
+    this.getPost();
 
     //login的post请求
     try {
