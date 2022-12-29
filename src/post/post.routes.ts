bac3388691929ac5a10943c1d788af4d4589a9d9
SHctt <SHcttMBG@protonmail.com>
2022-12-29 22:08:21 +0800

    内容页面：定义路由与组件

--- src/post/post.routes.ts
+++ src/post/post.routes.ts
@@ -1,5 +1,6 @@
 import { RouteRecordRaw } from 'vue-router';
 import PostsIndex from './index/posts-index.vue';
+import PostShow from './show/post-show.vue';
 
 // 创建posts路由
 const routes: Array<RouteRecordRaw> = [
@@ -7,6 +8,12 @@ const routes: Array<RouteRecordRaw> = [
     name: 'postsIndex',
     path: '/posts',
     component: PostsIndex,
+  },
+  {
+    name: 'postShow',
+    path: '/posts/:postId',
+    component: PostShow,
+    props: true,
   },
 ];
 
---
+++ src/post/show/post-show.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-if="showPost">
+    <h1>{{ post.title }}</h1>
+    <small>{{ post.user.name }}</small>
+    <div>{{ post.content }}</div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { mapActions, mapGetters } from 'vuex';
+
+export default defineComponent({
+  props: {
+    postId: String,
+  },
+
+  created() {
+    this.getPostById(this.postId);
+  },
+
+  computed: {
+    ...mapGetters({
+      loading: 'post/show/loading',
+      post: 'post/show/post',
+    }),
+    showPost() {
+      return !this.loading && this.post ? true : false;
+    },
+  },
+
+  methods: {
+    ...mapActions({
+      getPostById: 'post/show/getPostById',
+    }),
+  },
+});
+</script>
