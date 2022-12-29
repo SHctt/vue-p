03d41d82cefe032b960d1f066888e976c516c4c8
SHctt <SHcttMBG@protonmail.com>
2022-12-29 17:17:44 +0800

    Vue 应用的环境变量配置

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import { API_BASE_URL } from './app.config';
 
 // 创建并导出客户端，并为客户端增加相关设置
 export const appHttpClient = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: API_BASE_URL,
 });
