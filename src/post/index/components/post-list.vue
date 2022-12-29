9b8f586249abac272b01b353c77ad22b2d20d2dc
SHctt <SHcttMBG@protonmail.com>
2022-12-29 18:51:08 +0800

    在 Vue 组件内部请求服务接口获取组件需要的数据

--- src/post/index/components/post-list.vue
+++ src/post/index/components/post-list.vue
@@ -1,12 +1,29 @@
 <template>
-  <post-list-item></post-list-item>
+  <post-list-item
+    v-for="post in posts"
+    :item="post"
+    :key="post.id"
+  ></post-list-item>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 import postListItem from './post-list-item.vue';
+import { appHttpClient } from '@/app/app.service';
 
 export default defineComponent({
+  data() {
+    return {
+      posts: [],
+    };
+  },
+
+  async created() {
+    const response = await appHttpClient.get('/posts');
+
+    this.posts = response.data;
+  },
+
   components: { postListItem },
 });
 </script>
